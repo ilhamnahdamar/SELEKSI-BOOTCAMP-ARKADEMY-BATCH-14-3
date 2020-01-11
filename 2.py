251cb1f481d1bc593afa862a8b7232f7c271ba05
Ilham Ramadhan <32406616+ilhamramadhan21@users.noreply.github.com>
2020-01-11 20:44:18 +0700

    Jawaban Nomor 2

--- 2.py
+++ 2.py
@@ -1,21 +1,21 @@
-function requireUserName(name){
-  for (var a = 0; a < name.length; a++) {
-    if (name[a] !== name[a].toLowerCase()) {
-      return "Username anda tidak valid";
-    }
-  }
-  return 'Username valid'
+function isAcceptedUsername(username) {
+    var Regex = /^[aiueoAIUEO]{3}[a-z0-9]{5,12}$/i ;
+    return Regex.test(username) ;
 }
-console.log(requireUserName('arisupriatna'))
+// Cara menggunakan fungsi di atas
+if (isAcceptedUsername("aaat3st1ng")) {
+    alert("Username Is Valid") ;
+} else {
+    alert("Username Is Invalid") ;
+}
+
 
 function requirePassWord(pass) {
-
   for (var a = 0; a < pass.length; a++) {
     if (email[a] !== email[a].toUpperCase()) {
       return "Password anda tidak valid";
     }
   }
-
   var at = email.indexOf('0,1,2,3,4,5,6,7,8,9')
   if(at > 1){
     return 'Email anda valid'
