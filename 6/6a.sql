5984ee0ff3c8971ae71623aa785be3bd80aa7bb1
Ilham Ramadhan <32406616+ilhamramadhan21@users.noreply.github.com>
2020-01-11 22:05:18 +0700

    Jawaban Nomor 6A

--- 6/6a.sql
+++ 6/6a.sql
@@ -1,11 +1,38 @@
-Nama database : dbarkademy
-		- tb_product {
-			{id: 1, 2, 3}, {name: 'Latte', 'Cake', 'Comro'*MAKANAN FAVORIT*}, {price: 10000, 20000, 45000}, {id_category: 2, 1, 1}, {id_cashier: 1, 2, 3}
-			}
-		- tb_category {
-			{id: 1, 2}, {name: 'food', 'drink'}
-			}
-		- tb_cashier {
-			{id: 1 ,2 3}, {name: 'Pevita Pearce', 'Raisa Andriana', 'Ilham Ramadhan'*NAMA*}
+-- MEMBUAT DATABASE --
+CREATE DATABASE dbarkademy;
 
-SELECT dbarkademy FROM tb_product WHERE cashier, product, category, price
+-- MEMBUAT TABEL DAN MENGISI DATA PADA TABEL --
+CREATE TABLE tb_category (
+    id VARCHAR (20) NOT NULL,
+    name VARCHAR (50) NOT NULL,
+    PRIMARY KEY (id) );
+CREATE TABLE tb_cashier (
+    id VARCHAR (20) NOT NULL,
+    name VARCHAR (50) NOT NULL,
+    PRIMARY KEY (id) );
+/* id: 1 ,2 3
+name: 'Pevita Pearce', 'Raisa Andriana', 'Ilham Ramadhan'*NAMA*
+CREATE TABLE tb_product(
+    id VARCHAR (20) NOT NULL,
+    name VARCHAR (50) NOT NULL,
+    price VARCHAR(100) NOT NULL,
+    id_category VARCHAR(20) NOT NULL,
+    id_cashier VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id),
+    foreign key: id_category, id_cashier );
+/* id: 1, 2, 3
+
+-- MENGISI DATA PADA TABEL --
+INSERT INTO tb_product VALUES
+(1, 2),
+('Latte', 'Cake', 'Comro'),
+(10000, 20000, 45000);
+INSERT INTO tb_cashier VALUES
+(1, 2, 3),
+('Pevita Pearce', 'Raisa Andriana', 'Ilham Ramadhan');
+INSERT INTO tb_category VALUES
+(1, 2),
+('Food', 'Drink');
+
+-- MENAMPILKAN DATA PADA TABEL --
+SELECT cashier, product, category, price FROM tb_product;
