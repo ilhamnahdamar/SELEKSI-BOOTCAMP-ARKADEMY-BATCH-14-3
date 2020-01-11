40e118d79e962fed42bb2cc5ec27a0f4ea9bb245
Ilham Ramadhan <32406616+ilhamramadhan21@users.noreply.github.com>
2020-01-11 21:39:32 +0700

    Jawaban Nomor 6A

---
+++ 6/6a.sql
@@ -0,0 +1,11 @@
+Nama database : dbarkademy
+		- tb_product {
+			{id: 1, 2, 3}, {name: 'Latte', 'Cake', 'Comro'*MAKANAN FAVORIT*}, {price: 10000, 20000, 45000}, {id_category: 2, 1, 1}, {id_cashier: 1, 2, 3}
+			}
+		- tb_category {
+			{id: 1, 2}, {name: 'food', 'drink'}
+			}
+		- tb_cashier {
+			{id: 1 ,2 3}, {name: 'Pevita Pearce', 'Raisa Andriana', 'Ilham Ramadhan'*NAMA*}
+
+SELECT dbarkademy FROM tb_product WHERE cashier, product, category, price
