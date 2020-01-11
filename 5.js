eee453be5ba123e25308afb8dc006473114f17c5
Ilham Ramadhan <32406616+ilhamramadhan21@users.noreply.github.com>
2020-01-11 21:33:24 +0700

    Jawaban Nomor 5

--- 5.js
+++ 5.js
@@ -1,14 +1,6 @@
-#Buatlah sebuah fungsi untuk membuat sebuah array yang didalamnya terdiri dari deret angka bilangan ganji 1-10. Lalu jumlahkan semuanya.
-#Input		: randomize(5)
-#Output		: array	: [1,3,5,3,9]
-#		  sum 	: 23
-
-function dw(input){
-  console.log('DWDW          DW          DW') 
-  console.log('DW  DW        DW          DW')
-  console.log('DW    DW      DW    DW    DW')
-  console.log('DW    DW      DW  DWDWDW  DW')
-  console.log('DW  DW        DWDW      DWDW')
-  console.log('DWDW          DW          DW')
-}
-dw();
+n = 5
+hitung = 0
+while(n!= 10):
+  hitung += n
+  n= int(input("Masukan Nilai? "))
+print ("Jumlah: " , int(hitung))
